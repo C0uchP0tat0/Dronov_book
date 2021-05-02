f6f72063fc79f4cbc1ad481568d1624b1a27a9ab
unknown <denchik431@mail.ru>
2021-05-02 12:40:54 +0300

    Глава 10 стр.199

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import index, by_rubric, BbCreateView, add_and_save
+from .views import BbIndexView, BbCreateView, BbDetailView, BbByRubricView
+#from .views import index, by_rubric, add_and_save,
 
 urlpatterns = [
-    path ('add/', add_and_save, name='add'),
     path ('add/', BbCreateView.as_view(), name='add'),
-    path ('<int:rubric_id>/', by_rubric, name='by_rubric'),
-    path ('', index, name='index'),
+    path ('detail/<int:pk>/', BbDetailView.as_view(), name='detail'),
+    path ('<int:rubric_id>/', BbByRubricView.as_view(), name='by_rubric'),
+    path ('', BbIndexView.as_view(), name='index'),
+    #path ('', index, name='index'),
+    #path ('add/', add_and_save, name='add'),
+    #path ('<int:rubric_id>/', by_rubric, name='by_rubric'),
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -3,23 +3,35 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy, reverse
 from django.http import HttpResponseRedirect, HttpResponse, HttpRequest
 from django.template.response import TemplateResponse
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.views.generic.base import TemplateView
 
 from .models import Bb, Rubric
 from .forms import BbForm
 
-def index(request):
-    bbs = Bb.objects.order_by()
-    rubrics = Rubric.objects.all()
-    context = {'bbs': bbs, 'rubrics' : rubrics}
-    return TemplateResponse(request, 'bboard/index.html', context)
+class BbIndexView(TemplateView):
+    template_name = 'bboard/index.html'
 
-def by_rubric(request, rubric_id):
-    bbs = Bb.objects.filter(rubric=rubric_id)
-    rubrics = Rubric.objects.all()
-    current_rubric = Rubric.objects.get(pk=rubric_id)
-    context = {'bbs': bbs, 'rubrics' : rubrics, 'current_rubric': current_rubric}
-    return render (request, 'bboard/by_rubric.html', context) 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['bbs'] = Bb.objects.all()
+        context['rubrics'] = Rubric.objects.all()
+        return context        
 
+class BbByRubricView(ListView):
+    template_name = 'bboard/by_rubric.html'
+    context_object_name = 'bbs'
+
+    def get_queryset(self):
+        return Bb.objects.filter(rubric=self.kwargs['rubric_id'])
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        context['current_rubric'] = Rubric.objects.get(pk=self.kwargs['rubric_id'])
+        return context
+      
 class BbCreateView(CreateView):
     template_name = 'bboard/create.html'
     form_class = BbForm
@@ -30,7 +42,28 @@ class BbCreateView(CreateView):
         context['rubrics'] = Rubric.objects.all()
         return context
 
-def add_and_save(request):
+class BbDetailView(DetailView):
+    model = Bb
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+'''def index(request):
+    bbs = Bb.objects.order_by()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics' : rubrics}
+    return TemplateResponse(request, 'bboard/index.html', context)'''
+
+'''def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics' : rubrics, 'current_rubric': current_rubric}
+    return render (request, 'bboard/by_rubric.html', context)''' 
+
+'''def add_and_save(request):
     if request.method == 'POST':
         bbf=BbForm(request.POST)
         if bbf.is_valid():
@@ -43,4 +76,4 @@ def add_and_save(request):
     else:
         bbf=BbForm()
         context = {'form': bbf}
-        return render (request, 'bboard/create.html', context)
+        return render (request, 'bboard/create.html', context)'''
