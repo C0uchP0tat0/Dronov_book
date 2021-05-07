9d0f05abee3dcda72132c2b4cd985e6023de3425
unknown <denchik431@mail.ru>
2021-05-07 11:37:00 +0300

    Глава 11 стр.231

--- bboard/urls.py
+++ bboard/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path ('detail/<int:pk>/', BbDetailView.as_view(), name='detail'),
     path ('<int:rubric_id>/', BbByRubricView.as_view(), name='by_rubric'),
     path ('', BbIndexView.as_view(), name='index'),
+    #path ('detail/<int:year>/<int:month>/<int:day>/<int:pk>/', BbDetailView.as_view(), name='detail'),
     #path ('add/', BbAddView.as_view(), name='add'),
     #path ('', index, name='index'),
     #path ('add/', add_and_save, name='add'),
