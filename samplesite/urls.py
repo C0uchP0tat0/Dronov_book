d534c67bed9952c6a34f0cd3d849ebbda0b8a99f
unknown <denchik431@mail.ru>
2021-05-12 14:13:50 +0300

    Глава 15 стр. 303

--- samplesite/urls.py
+++ samplesite/urls.py
@@ -1,23 +1,11 @@
-"""samplesite URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from bboard.views import BbRedirectView
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView,\
-                                      PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView
+                                      PasswordChangeDoneView, PasswordResetView, \
+                                      PasswordResetDoneView, PasswordResetConfirmView,\
+                                      PasswordResetCompleteView
 
 urlpatterns = [
     path('', BbRedirectView.as_view(), name='old_index' ),
@@ -30,15 +18,20 @@ urlpatterns = [
          #template_name='registartion/change_password.html'), name='password_change'),
     #path('accounts/password_change/done/', PasswordChangeDoneView.as_view(
          #template_name='registartion/change_password.html'), name='password_change_done'),
-    path('accounts/password_reset', PasswordResetView.as_view(
+    path('accounts/password_reset', PasswordResetView.as_view(),
          #template_name='registartion/reset_password.html', 
-         subject_template_name='registartion/reset_subject.txt',
-         email_template_name='registartion/reset_email.html'),
+         #subject_template_name='registartion/reset_subject.txt',
+         #email_template_name='registartion/reset_email.html'),
          name='password_reset'),
     #path('accounts/password_reset/done/', PasswordResetDoneView.as_view(
          #template_name='registartion/email_sent.html'), name='password_reset_done'),
+    #path('accounts/reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(
+         #template_name='registartion/confirm_password.html'), name='password_reset_confirm'),
+    #path('accounts/reset/done/', PasswordResetCompleteView.as_view(
+         #template_name='registartion/password_confirmed.html'), name='password_reset_complete'),
     path('accounts/password_change/', PasswordChangeView.as_view(), name='password_change'),
     path('accounts/password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('accounts/password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-
+    path('accounts/reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
