83d9358d31344d44952007aa618445ced41a2e87
unknown <denchik431@mail.ru>
2021-05-18 11:12:33 +0300

    Часть 3.Глава 16.стр. 330

--- bboard/models.py
+++ bboard/models.py
@@ -7,12 +7,19 @@ class AdvUser(models.Model):
     is_activated = models.BooleanField(default=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+class BbManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().order_by('-published')
+
 class Bb(models.Model):
     title = models.CharField(max_length=50, verbose_name='Товар')
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликованно')
     rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
+    #Диспетчер обратной связи
+    objects = models.Manager()
+    revers = BbManager()
 
     def title_and_price(self):
         if self.price:
@@ -26,9 +33,17 @@ class Bb(models.Model):
         verbose_name = 'Объявление'
         ordering = ['-published']
 
+class RubricManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().order_by('order', 'name')
+
+    def order_by_bb_count(self):
+        return super().get_queryset().annotate(cnt=models.Count('bb')).order_by('-cnt')
+
 class Rubric(models.Model):
     name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
     order = models.SmallIntegerField(default=0, db_index=True)
+    objects = RubricManager()
 
     def __str__(self):
         return self.name
@@ -37,6 +52,27 @@ class Rubric(models.Model):
         verbose_name_plural = 'Рубрики'
         verbose_name = 'Рубрика'
         ordering = ['order', 'name'] 
+
+class RevRubric(Rubric):
+    class Meta:
+        proxy = True
+        ordering = ['-name']
+
+class Message(models.Model):
+    content = models.TextField()
+    #name = models.CharField(max_length=20)
+    #email = models.EmailField()
+
+    #lass Meta:
+        #abstract = True
+
+class PrivateMessage(Message):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Переопределим поле name
+    #name = models.CharField(max_length=40)
+    # Удаляем поле email
+    #email = None
+
 
 class Spare(models.Model):
     name = models.CharField(max_length=40)
