9b95b5152b978b9e8f15f69e2dc69ea607fc6218
unknown <denchik431@mail.ru>
2021-06-01 12:23:36 +0300

    Часть 3. Глава 19. стр. 374

--- bboard/templatetags/filtersandtags.py
+++ bboard/templatetags/filtersandtags.py
@@ -1,9 +1,18 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter
 def currency(value, name='руб.'):
     return '%1.2f %s' % (value, name)
+
+@register.simple_tag
+def lst(sep, *args):
+    return mark_safe('%s (итого <strong>%s</strong>)' % (sep.join(args), len(args)))
+
+@register.inclusion_tag('tags/ulist.html')
+def ulist(*args):
+    return {'items': args}
 
 #register.filter('currency', currency)
